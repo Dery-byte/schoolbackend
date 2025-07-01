91d0c903d2762894ee89336cd58f2839cffd895a
dery-byte <emmanueldery31@gmail.com>
2025-07-01 12:24:25 +0000

    Dockerfile

--- src/main/java/com/alibou/book/email/EmailService.java
+++ src/main/java/com/alibou/book/email/EmailService.java
@@ -53,7 +53,7 @@ public class EmailService {
         Context context = new Context();
         context.setVariables(properties);
 
-        helper.setFrom("emmanuelderryshare@gmail.com");
+        helper.setFrom("optimusinforservice@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
         System.out.println("Activation Link: " + confirmationUrl);
